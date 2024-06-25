5de60f8f41ada5f39ac55478a27ebf6794f7580d
Valéria de Araújo <araujovaleria.arte@gmail.com>
2024-06-25 14:39:02 -0300

    criação menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Menu = () => {
+    return (
+        <nav className="menu">
+            <ul className="menu-list">
+                <li className="menu-item"><a href="#">Home</a></li>
+                <li className="menu-item"><a href="#">Quem somos</a></li>
+                <li className="menu-item"><a href="#">Serviços</a></li>
+                <li className="menu-item"><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Menu;
